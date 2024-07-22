dd89f10dd9a02aeb95fc7760c4123e4f94dfb740
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-07-22 11:28:27 -0600

    Set available ships and start placeShip method

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -3,9 +3,14 @@ class Gameboard {
 
   #failedHits = 0;
 
+  #availableShips = 5;
+
   constructor() {
     this.#board = new Array(10)
       .fill()
       .map(() => new Array({ ship: null, hit: false }));
   }
+
+  // Methods
+  placeShip(ship, coordinates) {}
 }
