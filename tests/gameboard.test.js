ad0c1190e772761314834429d6ab383a713a62da
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-07-23 10:33:15 -0600

    Add Gameboard test to insert first ship

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,3 +1,16 @@
+import Coordinates from "../src/js/coordinates.js";
 import Gameboard from "../src/js/gameboard.js";
+import Ship from "../src/js/ship.js";
 
-describe("Gameboard tests", () => {});
+const gb = new Gameboard();
+
+describe("Gameboard tests", () => {
+  test("Place ship at any coordinates", () => {
+    const ship = new Ship(2);
+    gb.placeShip(ship, new Coordinates("A1"));
+
+    const allCellsFree = gb.isCellAvailable(0, 0) && gb.isCellAvailable(0, 1);
+
+    expect(allCellsFree).toBe(false);
+  });
+});
