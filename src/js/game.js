794346a12e89970fb91d07566d57aeb09110f647
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-07-28 12:20:03 -0600

    Add more ships

--- src/js/game.js
+++ src/js/game.js
@@ -24,9 +24,54 @@ function placeShips() {
       coordinates: new Coordinates("C3"),
       direction: "right",
     },
+    {
+      ship: new Ship(3),
+      coordinates: new Coordinates("H2"),
+      direction: "down",
+    },
+    {
+      ship: new Ship(4),
+      coordinates: new Coordinates("E6"),
+      direction: "right",
+    },
+    {
+      ship: new Ship(5),
+      coordinates: new Coordinates("F10"),
+      direction: "right",
+    },
   ]);
 
   updateBoard(players[0]);
+
+  players[1].setShips([
+    {
+      ship: new Ship(2),
+      coordinates: new Coordinates("B5"),
+      direction: "down",
+    },
+    {
+      ship: new Ship(2),
+      coordinates: new Coordinates("D3"),
+      direction: "right",
+    },
+    {
+      ship: new Ship(3),
+      coordinates: new Coordinates("G4"),
+      direction: "down",
+    },
+    {
+      ship: new Ship(4),
+      coordinates: new Coordinates("I6"),
+      direction: "down",
+    },
+    {
+      ship: new Ship(5),
+      coordinates: new Coordinates("C8"),
+      direction: "right",
+    },
+  ]);
+
+  updateBoard(players[1]);
 }
 
 function setupGame() {
