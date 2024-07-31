f667a88b1df4157f5603dc75dc5233db203448c3
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-07-30 18:10:00 -0600

    Convert game into a class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
-import { setupGame } from "./js/game";
+import Game from "./js/game";
 
-setupGame();
+const game = new Game();
+game.setupGame();
--- src/js/game.js
+++ src/js/game.js
@@ -15,98 +15,92 @@ import Coordinates from "./coordinates";
 8. Display variable information in the frontend
   */
 
-const players = [];
-let currentPlayer;
-let nextPlayer;
-let playerFlag = false;
-let state;
+class Game {
+  #players = [];
 
-function selectCell(cell, toPlayer) {
-  const coords = new Coordinates(cell.dataset.coordinates);
-  toPlayer.gameboard.receiveAttack(coords);
-  updateBoard(toPlayer);
+  #currentPlayer;
+
+  #nextPlayer;
+
+  #playerFlag = false;
+
+  #state;
+
+  placeShips() {
+    this.#players[0].setShips([
+      {
+        ship: new Ship(2),
+        coordinates: new Coordinates("A1"),
+        direction: "down",
+      },
+      {
+        ship: new Ship(2),
+        coordinates: new Coordinates("C3"),
+        direction: "right",
+      },
+      {
+        ship: new Ship(3),
+        coordinates: new Coordinates("H2"),
+        direction: "down",
+      },
+      {
+        ship: new Ship(4),
+        coordinates: new Coordinates("E6"),
+        direction: "right",
+      },
+      {
+        ship: new Ship(5),
+        coordinates: new Coordinates("F10"),
+        direction: "right",
+      },
+    ]);
+
+    updateBoard(this.#players[0]);
+
+    this.#players[1].setShips([
+      {
+        ship: new Ship(2),
+        coordinates: new Coordinates("B5"),
+        direction: "down",
+      },
+      {
+        ship: new Ship(2),
+        coordinates: new Coordinates("D3"),
+        direction: "right",
+      },
+      {
+        ship: new Ship(3),
+        coordinates: new Coordinates("G4"),
+        direction: "down",
+      },
+      {
+        ship: new Ship(4),
+        coordinates: new Coordinates("I6"),
+        direction: "down",
+      },
+      {
+        ship: new Ship(5),
+        coordinates: new Coordinates("C8"),
+        direction: "right",
+      },
+    ]);
+
+    updateBoard(this.#players[1]);
+  }
+
+  setupGame() {
+    this.#players.push(new Player("Player 1", 1));
+    this.#players.push(new Player("Player 2", 2));
+
+    this.#players.forEach((player) => createBoard(player));
+
+    // Set state to "Placing ships"
+    this.placeShips();
+
+    // Should be called when "Placing ships is finished"
+    // Set state to "Attacking"
+    this.startTurn();
+  }
 }
 
-function placeShips() {
-  players[0].setShips([
-    {
-      ship: new Ship(2),
-      coordinates: new Coordinates("A1"),
-      direction: "down",
-    },
-    {
-      ship: new Ship(2),
-      coordinates: new Coordinates("C3"),
-      direction: "right",
-    },
-    {
-      ship: new Ship(3),
-      coordinates: new Coordinates("H2"),
-      direction: "down",
-    },
-    {
-      ship: new Ship(4),
-      coordinates: new Coordinates("E6"),
-      direction: "right",
-    },
-    {
-      ship: new Ship(5),
-      coordinates: new Coordinates("F10"),
-      direction: "right",
-    },
-  ]);
-
-  updateBoard(players[0]);
-
-  players[1].setShips([
-    {
-      ship: new Ship(2),
-      coordinates: new Coordinates("B5"),
-      direction: "down",
-    },
-    {
-      ship: new Ship(2),
-      coordinates: new Coordinates("D3"),
-      direction: "right",
-    },
-    {
-      ship: new Ship(3),
-      coordinates: new Coordinates("G4"),
-      direction: "down",
-    },
-    {
-      ship: new Ship(4),
-      coordinates: new Coordinates("I6"),
-      direction: "down",
-    },
-    {
-      ship: new Ship(5),
-      coordinates: new Coordinates("C8"),
-      direction: "right",
-    },
-  ]);
-
-  updateBoard(players[1]);
-}
-
-function startTurn(player) {
-  currentPlayer = player;
-
-  setUpEventListeners(player);
-}
-
-function setupGame() {
-  players.push(new Player("Player 1", 1));
-  players.push(new Player("Player 2", 2));
-
-  players.forEach((player) => createBoard(player));
-
-  // Set state to "Placing ships"
-  placeShips();
-
-  // Should be called when "Placing ships is finished"
-  // Set state to "Attacking"
-  startTurn();
-}
-
-export { setupGame, selectCell };
+export default Game;
