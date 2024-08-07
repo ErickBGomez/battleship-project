86a0098aa632f4cc418289d52077941581fa7f63
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-08-07 16:33:55 -0600

    Create utils functions

--- src/js/players/computerPlayer.js
+++ src/js/players/computerPlayer.js
@@ -1,5 +1,6 @@
 import Coordinates from "../coordinates";
 import Player from "./player";
+import { randomRange } from "../utils";
 
 class ComputerPlayer extends Player {
   #difficulty;
@@ -14,13 +15,9 @@ class ComputerPlayer extends Player {
     return this.#difficulty;
   }
 
-  #randomRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
   selectRandomCoordinates() {
-    const column = this.#randomRange(0, 9);
-    const row = this.#randomRange(0, 9);
+    const column = randomRange(0, 9);
+    const row = randomRange(0, 9);
     return Coordinates.convert(column, row);
   }
 }
