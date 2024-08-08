40023cd9e05876dd32afa1d427aa61a30542a3a8
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-08-08 17:59:17 -0600

    Set validation of attack coordinates

--- src/js/players/computerPlayer.js
+++ src/js/players/computerPlayer.js
@@ -1,7 +1,8 @@
 import Coordinates from "../coordinates";
 import Player from "./player";
-import { randomRange } from "../utils";
+import { randomDirection, randomRange } from "../utils";
 import OutOfBoundsError from "../errors/outOfBoundsError";
+import Gameboard from "../gameboard";
 
 class ComputerPlayer extends Player {
   #difficulty;
@@ -22,11 +23,55 @@ class ComputerPlayer extends Player {
     this.#shipFoundPosition = value;
   }
 
-  // eslint-disable-next-line class-methods-use-this
   selectAttack() {
-    const column = randomRange(0, 9);
-    const row = randomRange(0, 9);
-    return Coordinates.convert(column, row);
+    let coords;
+    let column;
+    let row;
+    let invalidCoordinates = false;
+
+    do {
+      if (this.#shipFoundPosition && this.#difficulty === "hard") {
+        column = this.#shipFoundPosition.columnIndex;
+        row = this.#shipFoundPosition.rowIndex;
+
+        switch (randomDirection()) {
+          case "up":
+            row--;
+            break;
+
+          case "right":
+            column++;
+            break;
+
+          case "down":
+            row++;
+            break;
+
+          case "left":
+            column--;
+            break;
+
+          default:
+        }
+      } else {
+        column = randomRange(0, 9);
+        row = randomRange(0, 9);
+      }
+
+      try {
+        coords = Coordinates.convert(column, row);
+
+        if (!Gameboard.inBounds(column, row)) {
+          throw new OutOfBoundsError("Attack is out of bounds");
+        }
+
+        invalidCoordinates = false;
+      } catch {
+        invalidCoordinates = true;
+      }
+    } while (invalidCoordinates);
+
+    return coords;
   }
 }
 
