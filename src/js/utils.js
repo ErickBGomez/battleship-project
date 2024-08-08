4c7fdc64a0a7838eefd606367d0755e1c24037b8
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-08-07 19:05:24 -0600

    Function to get random directions in strings

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,4 +2,31 @@ function randomRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export { randomRange };
+function randomDirection() {
+  const dir = randomRange(0, 3);
+  let value;
+
+  switch (dir) {
+    case 0:
+      value = "up";
+      break;
+
+    case 1:
+      value = "right";
+      break;
+
+    case 2:
+      value = "down";
+      break;
+
+    case 3:
+      value = "left";
+      break;
+
+    default:
+  }
+
+  return value;
+}
+
+export { randomRange, randomDirection };
