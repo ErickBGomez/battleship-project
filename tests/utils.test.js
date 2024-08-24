7e70975775cdff62601cb394d9a6bbeef52e40c3
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-08-24 13:30:11 -0600

    Add parseTime tests with leading zeros

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,3 +1,4 @@
+import { parse } from "@babel/core";
 import { parseTime } from "../src/js/utils.js";
 
 describe("Parse time tests", () => {
@@ -7,5 +8,13 @@ describe("Parse time tests", () => {
 
   test("Should return 02:00", () => {
     expect(parseTime(120)).toBe("02:00");
+  });
+
+  test("Should return 00:59", () => {
+    expect(parseTime(59)).toBe("00:59");
+  });
+
+  test("Should return 05:06", () => {
+    expect(parseTime(306)).toBe("05:06");
   });
 });
