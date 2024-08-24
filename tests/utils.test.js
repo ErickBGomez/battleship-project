0dfb01f37cf2a4c8e78e8bfb49b5811cdb61655c
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-08-24 13:23:57 -0600

    Add more parseTime test

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,7 +1,11 @@
 import { parseTime } from "../src/js/utils.js";
 
 describe("Parse time tests", () => {
-  test("Should return 01:00", () => {
+  test("Should return 00:09", () => {
+    expect(parseTime(9)).toBe("00:09");
+  });
+
+  test("Should return 02:00", () => {
     expect(parseTime(120)).toBe("02:00");
   });
 });
