a00699a039d08a0c8305887fe0c34232147e1c27
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-08-24 13:30:38 -0600

    Add parseTime feature with formatting number with leading zeros

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,7 +2,7 @@ function parseTime(time) {
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
 
-  return `0${minutes}:0${seconds}`;
+  return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 }
 
 function randomRange(min, max) {
